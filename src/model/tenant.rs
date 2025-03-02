1c7f3e7c60843fc158aef37688c8ca1f717deddc
Nathan Faucett <nathanfaucett@gmail.com>
2025-03-02 13:59:38 -0500

    fix algorithm

--- src/model/tenant.rs
+++ src/model/tenant.rs
@@ -1,4 +1,4 @@
-use std::{fmt, str::FromStr};
+use std::{convert::Infallible, fmt, str::FromStr};
 
 use base64::{prelude::BASE64_STANDARD, Engine};
 use chrono::{DateTime, Utc};
@@ -16,7 +16,7 @@ pub struct Tenant {
   pub issuer: String,
   #[serde(skip_serializing_if = "Option::is_none")]
   pub audience: Option<String>,
-  pub algorithm: String,
+  pub algorithm: Algorithm,
   #[serde(skip_serializing_if = "Option::is_none")]
   pub public_key: Option<String>,
   #[serde(skip_serializing_if = "Option::is_none")]
@@ -35,7 +35,7 @@ impl From<TenantRow> for Tenant {
       client_id: uuid::Uuid::from_str(&row.client_id).unwrap_or_default(),
       issuer: row.issuer,
       audience: row.audience,
-      algorithm: row.algorithm,
+      algorithm: Algorithm::from_str(&row.algorithm).unwrap_or_default(),
       public_key: row.public_key,
       private_key: None,
       expires_in_seconds: row.expires_in_seconds,
@@ -54,7 +54,7 @@ pub struct TenantPagination {
 }
 
 /// jsonwebtoken::Algorithm
-#[derive(Deserialize, ToSchema, Default)]
+#[derive(Serialize, Deserialize, ToSchema, Default)]
 pub enum Algorithm {
   /// HMAC using SHA-256
   #[default]
@@ -110,6 +110,28 @@ impl Algorithm {
         public_key,
         private_key.unwrap_or_else(|| BASE64_STANDARD.encode(random_bytes(256))),
       ),
+    }
+  }
+}
+
+impl FromStr for Algorithm {
+  type Err = Infallible;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s.to_uppercase().as_str() {
+      "HS256" => Ok(Algorithm::HS256),
+      "HS384" => Ok(Algorithm::HS384),
+      "HS512" => Ok(Algorithm::HS512),
+      "ES256" => Ok(Algorithm::ES256),
+      "ES384" => Ok(Algorithm::ES384),
+      "RS256" => Ok(Algorithm::RS256),
+      "RS384" => Ok(Algorithm::RS384),
+      "RS512" => Ok(Algorithm::RS512),
+      "PS256" => Ok(Algorithm::PS256),
+      "PS384" => Ok(Algorithm::PS384),
+      "PS512" => Ok(Algorithm::PS512),
+      "EDDSA" => Ok(Algorithm::EdDSA),
+      _ => Ok(Algorithm::default()),
     }
   }
 }
